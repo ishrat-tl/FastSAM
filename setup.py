c9467cb3beeb6ff9b9919fe46bd2a8cbba716911
Ishrat Badami <99205142+ishrat-tl@users.noreply.github.com>
2023-07-26 16:14:39 -0400

    Update setup.py
    change utils to fastsam_utils

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[],
     package_dir= {
         "fastsam": "fastsam",
-        "fastsam_tools": "utils",
+        "fastsam_tools": "fastsam_utils",
     },
     url="https://github.com/CASIA-IVA-Lab/FastSAM",
 )
